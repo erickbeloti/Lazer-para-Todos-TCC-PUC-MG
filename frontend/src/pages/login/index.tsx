0ee1391687852ee5c433c7b08d4d4229ec47f3b3
Erick Beloti <erick.beloti@gmail.com>
2023-05-20 10:59:58 -0300

    Feature/next 13 pages folder (#1)
    * POC Next 13 + Pages Folder + Material UI

---
+++ frontend/src/pages/login/index.tsx
@@ -0,0 +1,78 @@
+import { Box, Container, Paper, TextField, Typography } from '@mui/material';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Unstable_Grid2';
+import Image from 'next/image';
+
+export default function Login() {
+	return (
+		<Container component="main" maxWidth="xs">
+			<Grid container direction={'column'}>
+				<Box sx={{ alignSelf: 'center', marginTop: 4 }}>
+					<Image src="/logo.svg" alt="logo" width="200" height="200" priority />
+				</Box>
+
+				<Paper
+					variant="outlined"
+					sx={{
+						borderRadius: '15px',
+						border: 0,
+					}}
+				>
+					<Paper
+						square
+						sx={{
+							backgroundColor: '#1AE6EF',
+							height: 50,
+							display: 'flex',
+							justifyContent: 'center',
+							borderTopLeftRadius: '15px',
+							borderTopRightRadius: '15px',
+						}}
+					>
+						<Grid container justifyContent="center" alignItems="center">
+							<Grid xs>
+								<Typography variant="h5">Login</Typography>
+							</Grid>
+						</Grid>
+					</Paper>
+					<Grid container direction={'column'}>
+						<TextField
+							label="Email"
+							variant="standard"
+							type="email"
+							sx={{
+								marginTop: '10px',
+								marginLeft: '50px',
+								marginRight: '50px',
+							}}
+						/>
+						<TextField
+							label="Senha"
+							variant="standard"
+							type="password"
+							sx={{
+								marginTop: '4px',
+								marginLeft: '50px',
+								marginRight: '50px',
+								marginBottom: '40px',
+							}}
+						/>
+					</Grid>
+				</Paper>
+				<Typography sx={{ alignSelf: 'center', marginTop: 0.5 }}>
+					Esqueceu sua senha?
+				</Typography>
+				<Button
+					variant="contained"
+					sx={{
+						marginTop: 2.5,
+						width: 128,
+						alignSelf: 'center',
+					}}
+				>
+					Entrar
+				</Button>
+			</Grid>
+		</Container>
+	);
+}
---
+++ frontend/src/theme.ts
@@ -0,0 +1,40 @@
+import { Roboto } from 'next/font/google';
+import { createTheme } from '@mui/material/styles';
+
+export const roboto = Roboto({
+	weight: ['300', '400', '500', '700'],
+	subsets: ['latin'],
+	display: 'swap',
+	fallback: ['Helvetica', 'Arial', 'sans-serif'],
+});
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: '#1AE6EF',
+		},
+		secondary: {
+			main: '#11cb5f',
+		},
+	},
+	typography: {
+		button: {
+			fontSize: 16,
+			fontWeight: 700,
+			textTransform: 'none',
+		},
+	},
+	components: {
+		MuiInputLabel: {
+			styleOverrides: {
+				root: {
+					'&.Mui-focused': {
+						color: 'rgb(0 0 0 / 60%)',
+					},
+				},
+			},
+		},
+	},
+});
+
+export default theme;
