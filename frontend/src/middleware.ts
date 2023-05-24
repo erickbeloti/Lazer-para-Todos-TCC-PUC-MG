9a873396da309a0d34047f95fc34f871862810e2
Erick Beloti <erick.beloti@gmail.com>
2023-05-23 23:30:06 -0300

    logs

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -8,33 +8,54 @@ export async function middleware(request: NextRequest, _next: NextFetchEvent) {
 		path => pathname.startsWith(path) || pathname === '/',
 	);
 
+	console.log(pathname);
+
 	if (matchesProtectedPath) {
 		const token = await getToken({ req: request });
 		if (!token) {
 			const url = new URL(`/signin`, request.url);
 			url.searchParams.set('callbackUrl', encodeURI(request.url));
+
+			console.log('sem login');
+			console.log(url);
+
 			return NextResponse.redirect(url);
 		}
 
 		if (token?.error === 'expired token') {
-			console.log('token expirado');
 			const url = new URL(`/signin`, request.url);
 			url.searchParams.set('callbackUrl', encodeURI(request.url));
+
+			console.log('token expirado');
+			console.log(url);
+
 			return NextResponse.redirect(url);
 		}
 
 		if (token.userRole === 'pcd' && pathname === '/') {
 			const url = new URL(`/pcd`, request.url);
+
+			console.log('role pcd');
+			console.log(url);
+
 			return NextResponse.redirect(url);
 		}
 
 		if (token.userRole === 'pcd' && !pathname.startsWith('/pcd')) {
 			const url = new URL(`/403`, request.url);
+
+			console.log('role pcd e não começa com pcd');
+			console.log(url);
+
 			return NextResponse.rewrite(url);
 		}
 
 		if (token.userRole === 'proprietario' && pathname === '/') {
 			const url = new URL(`/proprietario`, request.url);
+
+			console.log('role proprietario');
+			console.log(url);
+
 			return NextResponse.redirect(url);
 		}
 
@@ -43,11 +64,19 @@ export async function middleware(request: NextRequest, _next: NextFetchEvent) {
 			!pathname.startsWith('/proprietario')
 		) {
 			const url = new URL(`/403`, request.url);
+
+			console.log('role proprietario e não começa com proprietario');
+			console.log(url);
+
 			return NextResponse.rewrite(url);
 		}
 
 		if (!token.userRole) {
 			const url = new URL(`/403`, request.url);
+
+			console.log('403');
+			console.log(url);
+
 			return NextResponse.rewrite(url);
 		}
 	}
