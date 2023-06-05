51c015c032f9f2f16d37581b77b12d84f5e8e728
vmafer <vilelafernando17@gmail.com>
2023-06-05 00:52:12 -0300

    Proprietario update, delete e filtos avancados

--- backend/src/main/java/com/projetopuc/lazerparatodos/controller/ProprietarioController.java
+++ backend/src/main/java/com/projetopuc/lazerparatodos/controller/ProprietarioController.java
@@ -2,6 +2,7 @@ package com.projetopuc.lazerparatodos.controller;
 
 import com.projetopuc.lazerparatodos.models.entities.Proprietario;
 import com.projetopuc.lazerparatodos.models.repositories.ProprietarioRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,31 +14,43 @@ import java.util.Optional;
 @RequestMapping("/api/proprietarios")
 public class ProprietarioController {
 
-    @GetMapping
-    public List<Proprietario> getallProprietarios(){
-        return proprietarioRepository.findAll();
-    }
-
-    /*@GetMapping(path = "/{name}")
-    public List<Proprietario> getProprietariosbyName(@PathVariable String name){
-        Proprietario proprietario = proprietarioRepository.findBy()
-
-        return null;
-    }*/
-
-    @GetMapping(path = "/{id}")
-    public Proprietario getProprietarioById(@PathVariable int id){
-        Optional<Proprietario> proprietario = proprietarioRepository.findById(id);
-            return proprietario.stream().findFirst().get();
-
-    }
-
     @PostMapping
     public @ResponseBody Proprietario saveProprietario(Proprietario proprietario){
         proprietarioRepository.save(proprietario);
         return proprietario;
     }
 
+    @GetMapping
+    public List<Proprietario> getallProprietarios(){
+        return proprietarioRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Optional<Proprietario> getProprietarioById(@PathVariable int id){
+        return proprietarioRepository.findById(id);
+    }
+
+    @GetMapping(path = "/filter/{nome}")
+    public List<Proprietario> getProprietariosbyName(@PathVariable String nome){
+        return proprietarioRepository.findBynomeEstabelecimento(nome);
+    }
+
+    @GetMapping(path = "/filteravancado/{nome}/{local}")
+    public List<Proprietario>getProprietarioFiltroAvancado(@PathVariable String nome, @PathVariable String local){
+        return proprietarioRepository.findProprieratioByfilter(nome, local);
+    }
+
+    @PutMapping(path = "/{id}")
+    public @ResponseBody Proprietario updateProprietario(Proprietario proprietario){
+        proprietarioRepository.save(proprietario);
+        return proprietario;
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public @ResponseBody Proprietario deleteProprietario(Proprietario proprietario){
+        proprietarioRepository.delete(proprietario);
+        return proprietario;
+    }
 
     @Autowired
     ProprietarioRepository proprietarioRepository;
--- backend/src/main/java/com/projetopuc/lazerparatodos/models/repositories/ProprietarioRepository.java
+++ backend/src/main/java/com/projetopuc/lazerparatodos/models/repositories/ProprietarioRepository.java
@@ -2,8 +2,19 @@ package com.projetopuc.lazerparatodos.models.repositories;
 
 import com.projetopuc.lazerparatodos.models.entities.Proprietario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+import java.util.Optional;
 
 
 public interface ProprietarioRepository extends JpaRepository<Proprietario, Integer> {
+
+    public List<Proprietario> findBynomeEstabelecimento(String nomeEstabelecimento);
+
+    @Query("SELECT p FROM Proprietario p WHERE p.nomeEstabelecimento = :nome and p.logadouro = :local")
+    public List<Proprietario> findProprieratioByfilter(@Param("nome") String nomeEstabelecimento,
+                                         @Param("local") String logadouro);
+
 }
