0ab6bf1fe66dbb4b6bc5b099ce01344079e14081
Erick <erick.beloti@gmail.com>
2023-06-27 14:37:50 -0300

    Adicionado resposta de avaliação média e deficiencias confirmadas no retorno do prestador

--- backend/src/main/java/com/projetopuc/lazerparatodos/repositories/ComentarioRepository.java
+++ backend/src/main/java/com/projetopuc/lazerparatodos/repositories/ComentarioRepository.java
@@ -4,6 +4,7 @@ import com.projetopuc.lazerparatodos.entities.Comentario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface ComentarioRepository extends JpaRepository<Comentario, Integer> {
@@ -12,4 +13,12 @@ public interface ComentarioRepository extends JpaRepository<Comentario, Integer>
             " LEFT JOIN FETCH c.pcd" +
             " WHERE c.proprietario.id = :proprietarioId")
     List<Comentario> findAllByProprietarioId(Integer proprietarioId);
+
+    @Query("SELECT avg(c.avaliacao) FROM Comentario c WHERE c.proprietario.id = :proprietarioId")
+    BigDecimal getAvaliacaoMedia(Integer proprietarioId);
+
+    @Query("SELECT distinct(d.id) FROM Comentario c" +
+            " LEFT JOIN c.deficiencias d" +
+            " WHERE c.proprietario.id = :proprietarioId")
+    List<Integer> findDistinctByProprietarioId(Integer proprietarioId);
 }
