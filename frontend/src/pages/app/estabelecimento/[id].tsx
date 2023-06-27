d9bde439c231166ae9c1dd713da44fa668ddd01e
Erick Beloti <erick.beloti@gmail.com>
2023-06-27 12:04:18 -0300

    Feature/integracao findbyid estabelecimento (#33)
    * Adicionado componente para listar Comentarios
    
    * Adicionado componente ImageProprietario
    
    * Adicionado novas propriedades para o componente Disability
    
    * Realizado integração findbyid do proprietário

--- frontend/src/pages/app/estabelecimento/[id].tsx
+++ frontend/src/pages/app/estabelecimento/[id].tsx
@@ -1,367 +1,189 @@
 import {
+	Alert,
 	Avatar,
+	Backdrop,
 	Box,
 	Button,
+	CircularProgress,
 	Container,
 	Paper,
 	Rating,
 	Typography,
 } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
-import Image from 'next/image';
-import Carousel from 'react-material-ui-carousel';
-import AuditivaSvg from '../../../../public/disabilities/auditiva.svg';
-import AutismoSvg from '../../../../public/disabilities/autismo.svg';
-import FisicaSvg from '../../../../public/disabilities/fisica.svg';
-import Idoso80Svg from '../../../../public/disabilities/idoso80.svg';
-import IntelectualSvg from '../../../../public/disabilities/intelectual.svg';
-import VisualSvg from '../../../../public/disabilities/visual.svg';
 import Head from 'next/head';
+import useSWR from 'swr';
+import { useRouter } from 'next/router';
+import api from '@/services/api';
+import ImagemProprietario from '@/components/proprietario/ImageProprietario';
+import DisabilityIcon from '@/components/disability';
+import Comentario from '@/components/proprietario/Comentario';
 
-const items = [
-	{
-		url: '/companies/1/1.png',
-	},
-	{
-		url: '/companies/1/2.png',
-	},
-	{
-		url: '/companies/1/3.png',
-	},
-	{
-		url: '/companies/1/4.png',
-	},
-];
-
-interface Item {
-	item: {
-		url: string;
-	};
-}
-
-function Item(props: Item) {
-	return (
-		<Box position={'relative'} height={300}>
-			<Image src={props.item.url} alt="logo" fill priority />
-		</Box>
-	);
-}
+const fetcher = (url: string) => api.get(url).then(res => res.data);
 
 export default function Estabelecimento() {
+	const { query, asPath } = useRouter();
+	const { id } = query;
+
+	const {
+		data: proprietario,
+		error,
+		isLoading,
+	} = useSWR<ProprietarioUserApiType>(
+		id ? `/api/proprietarios/${id}` : null,
+		fetcher,
+	);
+
+	if (!isLoading && id && error)
+		return (
+			<Container component="main" maxWidth="sm">
+				<Box m={2}>
+					<Alert severity="error" sx={{ textAlign: 'center' }}>
+						Erro ao tentar recuperar informações do estabelecimento.
+						<Button href={asPath}>Tente novamente</Button>
+					</Alert>
+				</Box>
+			</Container>
+		);
+
 	return (
 		<>
 			<Head>
 				<title>Estabelecimento</title>
 			</Head>
-			<Container component="main" maxWidth="md">
-				<Box mt={2} />
-				<Paper
-					variant="outlined"
-					sx={{
-						borderRadius: '15px',
-						border: 0,
-						position: 'relative',
-					}}
+
+			{(isLoading || !id) && (
+				<Backdrop
+					sx={{ color: '#fff', zIndex: theme => theme.zIndex.drawer + 1 }}
+					open
 				>
-					<Grid container alignItems={'center'} gap={1} pt={1}>
-						<Grid xs={12} md={1} pl={1}>
-							<Avatar
-								alt="Pão e Prosa"
-								src="/companies/1/1.png"
-								sx={{ width: 70, height: 70 }}
+					<CircularProgress color="inherit" />
+				</Backdrop>
+			)}
+
+			{!isLoading && id && (
+				<Container component="main" maxWidth="md">
+					<Box mt={2} />
+					<Paper
+						variant="outlined"
+						sx={{
+							borderRadius: '15px',
+							border: 0,
+							position: 'relative',
+						}}
+					>
+						<Grid container alignItems={'center'} gap={1} pt={1}>
+							<Grid xs={12} md={1} pl={1}>
+								<Avatar
+									alt={proprietario?.nomeEstabelecimento}
+									src={proprietario?.urlIcone}
+									sx={{ width: 70, height: 70 }}
+								/>
+							</Grid>
+
+							<Grid xs={12} md>
+								<Grid container direction={'column'} pl={1}>
+									<Typography variant="h6" fontWeight={700}>
+										{proprietario?.nomeEstabelecimento}
+									</Typography>
+									<Typography
+										variant="subtitle2"
+										fontWeight={700}
+										color={'rgb(0 0 0 / 41%)'}
+									>
+										{`${proprietario?.logradouro}, ${proprietario?.endereco.bairro}, ${proprietario?.endereco.cidade} - ${proprietario?.endereco.estado}`}
+									</Typography>
+									<Typography
+										variant="subtitle2"
+										fontWeight={700}
+										color={'rgb(0 0 0 / 41%)'}
+									>
+										{proprietario?.telefone}
+									</Typography>
+
+									<Box position={'absolute'} top={8} right={8}>
+										<Button
+											variant="contained"
+											color="secondary"
+											sx={{
+												width: 103,
+											}}
+										>
+											Seguir
+										</Button>
+									</Box>
+								</Grid>
+							</Grid>
+						</Grid>
+
+						<Grid container justifyContent={'end'} mr={1}>
+							<Rating
+								precision={0.5}
+								size="large"
+								value={proprietario?.avaliacaoMedia}
+								readOnly
 							/>
 						</Grid>
 
-						<Grid xs={12} md>
-							<Grid container direction={'column'} pl={1}>
-								<Typography variant="h6" fontWeight={700}>
-									Pão e Prosa
-								</Typography>
-								<Typography
-									variant="subtitle2"
-									fontWeight={700}
-									color={'rgb(0 0 0 / 41%)'}
-								>
-									Rua dos Poetas, 123 - Vila das Artes, São Paulo - SP,
-									01234-567
-								</Typography>
-								<Typography
-									variant="subtitle2"
-									fontWeight={700}
-									color={'rgb(0 0 0 / 41%)'}
-								>
-									(11) 5555-5555
-								</Typography>
+						<ImagemProprietario
+							proprietario={proprietario}
+							showEditButton={false}
+							showAvatarButton={false}
+							sx={{ borderRadius: 0 }}
+						/>
 
-								<Box position={'absolute'} top={8} right={8}>
-									<Button
-										variant="contained"
-										color="secondary"
-										sx={{
-											width: 103,
-										}}
-									>
-										Seguir
-									</Button>
-								</Box>
-							</Grid>
-						</Grid>
-					</Grid>
+						<Box p={2}>
+							<Typography
+								variant="subtitle2"
+								fontWeight={700}
+								color={'rgb(0 0 0 / 41%)'}
+							>
+								Acessibilidades
+							</Typography>
 
-					<Grid container justifyContent={'end'} mr={1}>
-						<Rating precision={0.5} size="large" value={4} readOnly />
-					</Grid>
+							{proprietario?.deficiencias.map(({ deficiencia, confirmada }) => (
+								<DisabilityIcon
+									key={deficiencia.id}
+									type={deficiencia.tipoDeDeficiencia}
+									elabled={confirmada}
+								/>
+							))}
 
-					<Carousel navButtonsAlwaysVisible>
-						{items.map((item, i) => (
-							<Item key={i} item={item} />
-						))}
-					</Carousel>
+							<Typography variant="h6" fontWeight={700}>
+								Descrição
+							</Typography>
 
-					<Box p={2}>
-						<Typography
-							variant="subtitle2"
-							fontWeight={700}
-							color={'rgb(0 0 0 / 41%)'}
-						>
-							Acessibilidades confirmadas
-						</Typography>
+							<Typography variant="subtitle2">
+								{proprietario?.descricao}
+							</Typography>
+						</Box>
+					</Paper>
 
-						<FisicaSvg width={40} height={40} />
+					<Box mt={2} />
 
-						<VisualSvg width={40} height={40} />
+					<Paper
+						variant="outlined"
+						sx={{
+							borderRadius: '15px',
+							border: 0,
+							p: 2,
+							display: 'flex',
+							flexDirection: 'column',
+							gap: 3,
+						}}
+					>
+						{proprietario?.comentarios.length === 0 ? (
+							<Typography variant="h6">Nenhum comentário</Typography>
+						) : (
+							proprietario?.comentarios.map(comentario => (
+								<Comentario key={comentario.id} comentario={comentario} />
+							))
+						)}
+					</Paper>
 
-						<AuditivaSvg fill="rgb(0 0 0 / 35%)" width={40} height={40} />
-
-						<Idoso80Svg width={40} height={40} />
-
-						<IntelectualSvg fill="rgb(0 0 0 / 35%)" width={40} height={40} />
-
-						<AutismoSvg fill="rgb(0 0 0 / 35%)" width={40} height={40} />
-
-						<Typography variant="h6" fontWeight={700}>
-							Descrição
-						</Typography>
-
-						<Typography variant="subtitle2">
-							A cafeteria Pão e Prosa é um ambiente acolhedor e inclusivo,
-							pensado para receber pessoas de todas as habilidades e
-							necessidades. Com cardápio variado, que inclui opções sem glúten e
-							sem lactose, e ambiente acessível, a cafeteria busca proporcionar
-							uma experiência agradável para todos os clientes. Além disso, a
-							Pão e Prosa valoriza a diversidade e a inclusão, buscando promover
-							um ambiente de convivência harmônica e respeitosa.
-						</Typography>
-					</Box>
-				</Paper>
-
-				<Box mt={2} />
-
-				<Paper
-					variant="outlined"
-					sx={{
-						borderRadius: '15px',
-						border: 0,
-						p: 2,
-						display: 'flex',
-						flexDirection: 'column',
-						gap: 3,
-					}}
-				>
-					<Box>
-						<Typography variant="h6" fontWeight={700}>
-							Ana Silva
-						</Typography>
-
-						<Paper
-							variant="outlined"
-							sx={{
-								borderRadius: '15px',
-								border: 0,
-								backgroundColor: '#6B3B82',
-								color: '#fff',
-								p: 2,
-							}}
-						>
-							<Grid container justifyContent={'end'}>
-								<Grid xs={12} md>
-									<Typography variant="subtitle2">
-										A Pão e Prosa é um lugar super aconchegante, com uma
-										decoração rústica que cria um ambiente convidativo para
-										todos. Gostei especialmente da disponibilidade de rampas e
-										corrimões para pessoas com mobilidade reduzida, o que mostra
-										que eles se preocupam com a acessibilidade. A única coisa
-										que poderia ser melhor é o tamanho da fonte no cardápio, que
-										é um pouco pequena para quem tem dificuldades visuais.
-									</Typography>
-								</Grid>
-
-								<Grid container direction={'column'}>
-									<Grid>
-										<Rating precision={0.5} size="large" value={4} readOnly />
-									</Grid>
-
-									<Grid container>
-										<FisicaSvg fill="rgb(0 0 0 / 35%)" width={25} height={25} />
-
-										<VisualSvg fill="rgb(0 0 0 / 35%)" width={25} height={25} />
-
-										<AuditivaSvg
-											fill="rgb(0 0 0 / 35%)"
-											width={25}
-											height={25}
-										/>
-
-										<Idoso80Svg width={25} height={25} />
-
-										<IntelectualSvg
-											fill="rgb(0 0 0 / 35%)"
-											width={25}
-											height={25}
-										/>
-
-										<AutismoSvg
-											fill="rgb(0 0 0 / 35%)"
-											width={25}
-											height={25}
-										/>
-									</Grid>
-								</Grid>
-							</Grid>
-						</Paper>
-					</Box>
-
-					<Box>
-						<Typography variant="h6" fontWeight={700}>
-							Maria Oliveira
-						</Typography>
-
-						<Paper
-							variant="outlined"
-							sx={{
-								borderRadius: '15px',
-								border: 0,
-								backgroundColor: '#6B3B82',
-								color: '#fff',
-								p: 2,
-							}}
-						>
-							<Grid container justifyContent={'end'}>
-								<Grid xs={12} md>
-									<Typography variant="subtitle2">
-										Sou apaixonada por essa cafeteria! Além de um ambiente
-										charmoso, eles possuem cardápio com opções veganas e sem
-										glúten, que é ótimo para quem tem restrições alimentares. Eu
-										também gostei da sinalização em braile nos cardápios, o que
-										torna o lugar ainda mais inclusivo.
-									</Typography>
-								</Grid>
-
-								<Grid container direction={'column'}>
-									<Grid>
-										<Rating precision={0.5} size="large" value={5} readOnly />
-									</Grid>
-
-									<Grid container>
-										<FisicaSvg fill="rgb(0 0 0 / 35%)" width={25} height={25} />
-
-										<VisualSvg width={25} height={25} />
-
-										<AuditivaSvg
-											fill="rgb(0 0 0 / 35%)"
-											width={25}
-											height={25}
-										/>
-
-										<Idoso80Svg
-											fill="rgb(0 0 0 / 35%)"
-											width={25}
-											height={25}
-										/>
-
-										<IntelectualSvg
-											fill="rgb(0 0 0 / 35%)"
-											width={25}
-											height={25}
-										/>
-
-										<AutismoSvg
-											fill="rgb(0 0 0 / 35%)"
-											width={25}
-											height={25}
-										/>
-									</Grid>
-								</Grid>
-							</Grid>
-						</Paper>
-					</Box>
-
-					<Box>
-						<Typography variant="h6" fontWeight={700}>
-							Pedro Rocha
-						</Typography>
-
-						<Paper
-							variant="outlined"
-							sx={{
-								borderRadius: '15px',
-								border: 0,
-								backgroundColor: '#6B3B82',
-								color: '#fff',
-								p: 2,
-							}}
-						>
-							<Grid container justifyContent={'end'}>
-								<Grid xs={12} md>
-									<Typography variant="subtitle2">
-										A Pão e Prosa tem um café delicioso, mas senti falta de mais
-										opções de cadeiras e mesas acessíveis para cadeirantes. No
-										entanto, fiquei feliz em ver que eles possuem um banheiro
-										acessível, que é um item básico de acessibilidade que muitos
-										lugares ignoram.
-									</Typography>
-								</Grid>
-
-								<Grid container direction={'column'}>
-									<Grid>
-										<Rating precision={0.5} size="large" value={3} readOnly />
-									</Grid>
-
-									<Grid container>
-										<FisicaSvg width={25} height={25} />
-
-										<VisualSvg fill="rgb(0 0 0 / 35%)" width={25} height={25} />
-
-										<AuditivaSvg
-											fill="rgb(0 0 0 / 35%)"
-											width={25}
-											height={25}
-										/>
-
-										<Idoso80Svg
-											fill="rgb(0 0 0 / 35%)"
-											width={25}
-											height={25}
-										/>
-
-										<IntelectualSvg
-											fill="rgb(0 0 0 / 35%)"
-											width={25}
-											height={25}
-										/>
-
-										<AutismoSvg
-											fill="rgb(0 0 0 / 35%)"
-											width={25}
-											height={25}
-										/>
-									</Grid>
-								</Grid>
-							</Grid>
-						</Paper>
-					</Box>
-				</Paper>
-			</Container>
+					<Box mb={2} />
+				</Container>
+			)}
 		</>
 	);
 }
